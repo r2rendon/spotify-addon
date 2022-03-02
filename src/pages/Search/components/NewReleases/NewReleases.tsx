bb59b03d95fcf91cd3ad8a1e632a64652b3579a0
Arturo Rendon <arturo.rendon@unitec.edu>
2022-03-02 00:48:40 -0600

    Add NewReleases can add to library

--- src/pages/Search/components/NewReleases/NewReleases.tsx
+++ src/pages/Search/components/NewReleases/NewReleases.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./NewReleases.css";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../../../firebase/config";
 
 const PLAYLISTS_ENDPOINT = "https://api.spotify.com/v1/me/playlists";
 
 const NewReleases = () => {
-  const [tracks, setTracks] = useState<any[]>();
+  const [tracks, setTracks] = useState<any[]>([]);
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
@@ -33,17 +35,42 @@ const NewReleases = () => {
 
   }, []);
 
+  const addTrackToLibrary = (e:any, trackIndex: number) => {
+    e.preventDefault();
+    const username = localStorage.getItem("username");
+    if (username) {
+      const writeTrackToLibrary = async (track: any) => {
+
+        const trackInfo = {
+          album: track.album.name,
+          name: track.name,
+          albumImage: track.album.images[0].url,
+          id: track.id,
+        };
+
+        await addDoc(collection(db, "libraries", username, "tracks"), trackInfo);
+      }
+      writeTrackToLibrary(tracks[trackIndex]);
+    }
+  }
+
   return (
     <>
       <h2>New Releases</h2>
       <div className="row mt-3">
-        {tracks?.map((track) => (
+        {tracks?.map((track, index) => (
           <div className="col-md-3 d-flex justify-content-center">
             <div className="shadow-lg card bg-dark text-white">
               <img src={track?.album.images[0].url} alt="" className="card-img-top" />
               <div className="card-body">
                 <div className="card-text-container">
-                  <p className="card-text text-center">{track?.name}</p>
+                  <p className="card-text text-center">
+                    {track?.name}
+                    <br />
+                    <span onClick={e => addTrackToLibrary(e, index)}>
+                      - Add to library
+                    </span>  
+                  </p>
                 </div>
               </div>
             </div>
